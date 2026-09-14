31980d080998a9832568b9ef2998fffa4bec4450
mgjonathanlopez-sys <mgjonathanlopez@gmail.com>
2026-09-14 12:01:39 -0500

    Actualizar generador del ejecutable genérico

--- Crear_EXE.bat
+++ Crear_EXE.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal
-title Crear EXE - Redes que Transforman
+title Crear EXE - Gestor de Proyectos de Investigacion
 cd /d "%~dp0"
 if not exist ".venv\Scripts\python.exe" py -3 -m venv .venv
-".venv\Scripts\python.exe" -m pip install --upgrade pip pyinstaller
+".venv\Scripts\python.exe" -m pip install --upgrade pip
+".venv\Scripts\python.exe" -m pip install -r requirements.txt pyinstaller
 if errorlevel 1 goto error
-".venv\Scripts\python.exe" -m PyInstaller --noconfirm --clean --onefile --windowed --name "Redes_que_Transforman" "redes_transforman.py"
+".venv\Scripts\python.exe" -m PyInstaller --noconfirm --clean --onefile --windowed --name "Gestor_Proyectos_Investigacion" "gestor_proyectos.py"
 if errorlevel 1 goto error
-echo.
-echo EXE creado en: %CD%\dist\Redes_que_Transforman.exe
+echo Ejecutable creado en %CD%\dist\Gestor_Proyectos_Investigacion.exe
 explorer "%CD%\dist"
 pause
 exit /b 0
@@ -16,4 +16,3 @@ exit /b 0
 echo No fue posible generar el ejecutable.
 pause
 exit /b 1
-endlocal
