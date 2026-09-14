43977604ccdc82ffd8c5577095f174a2f1672bb1
mgjonathanlopez-sys <mgjonathanlopez@gmail.com>
2026-09-14 12:01:54 -0500

    Eliminar iniciador obsoleto

--- Iniciar_Redes_Transforman.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-if exist ".venv\Scripts\pythonw.exe" (
-  start "" ".venv\Scripts\pythonw.exe" "redes_transforman.py"
-) else (
-  py -3 "redes_transforman.py"
-)
-endlocal
